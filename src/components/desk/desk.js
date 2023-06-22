bb2cb0278736878ff86949c0878445b8d6ecc925
Miron Arystan <miron@MacBook-Air-Miron.local>
2023-06-22 15:08:33 +0600

    fix zIndex when editing

--- src/components/desk/desk.js
+++ src/components/desk/desk.js
@@ -45,9 +45,8 @@ export default function Desk() {
       endIndex = tasks.findIndex((t) => t.id === endIndexId);
     }
 
-    // FIXME
-    // console.log(dstIndex, setUpper);
-    // console.log(tasks[startIndex].description, tasks[endIndex].description);
+    console.log(dstIndex);
+    console.log(tasks[startIndex].description, tasks[endIndex].description);
 
     // reorder
     reorder(endIndex, startIndex, isNewColumn, dstDroppableId);
